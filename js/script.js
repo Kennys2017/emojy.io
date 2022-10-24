a2bd673352b2c419fd97039d2dc64231232c0e62
Kennys2017 <102148024+Kennys2017@users.noreply.github.com>
2022-10-24 12:38:31 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,28 +3,27 @@ let getJsonFile = (pathToFile) => {
     request.open("GET", pathToFile, false);
     request.send(null);
     let my_JSON_object = JSON.parse(request.responseText);
-
     return my_JSON_object;
 };
 let stroke = '';
-let emoji = document.getElementsByClassName("searchBlock")[0];
+let emoji = document.getElementsByClassName("searchBlock")[0];  //Document представляет собой любую веб-страницу, загруженную в браузер, и служит точкой входа в содержимое веб-страницы, которое представляет собой дерево DOM 
 const allEmoji = getJsonFile("https://emoji-api-app.herokuapp.com/");
-str(allEmoji);
+str(allEmoji); // преобразование в строку
     function str(array){
         stroke = '';
     for(let i = 0; i<array.length;i++){
         stroke += 
-      `<div  class ="searchBlob">
+      `<div  class ="searchBlob"> //создание блока с эмоджи
       <h2 class="symbol">${array[i].symbol}</h2>
       <h3>${array[i].title}</h3>
       <p>${array[i].keywords}</p>
          </div>`;      
     }
-    emoji.innerHTML = stroke;
+    emoji.innerHTML = stroke; //Свойство интерфейса Element innerHTML устанавливает или получает HTML или XML разметку дочерних элементов.
 }
    
-    document.querySelector("#place").oninput = function(){
-    let value = this.value.trim();
+    document.querySelector("#place").oninput = function(){  //метод querySelector() возвращает первый элемент (Element) документа, который соответствует указанному селектору или группе селекторов. Если совпадений не найдено, возвращает значение null.
+    let value = this.value.trim(); //Свойство контекста выполнения кода, которое в нестрогом режиме всегда является ссылкой на объект, а в строгом режиме может иметь любое значение.
     let item = allEmoji.filter(element => element.title.includes(value) || element.keywords.includes(value));
         str(item);
 }
